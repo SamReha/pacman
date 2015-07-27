42401b8db8207e7492197f9e96c4afa4031b3d80
Sam Reha <sreha@ucsc.edu>
2015-07-26 17:33:24 -0700

    Completed initial map implementation

--- pacman/map.cpp
+++ pacman/map.cpp
@@ -10,8 +10,60 @@ Map::Map(sf::Vector2<double> anchor, sf::Vector2<int> sze, int width) {
 }
 
 // Private
+// Compute index from point (in pixels), anchorPoint, and tileWidth
+int Map::getIndexFromPoint(sf::Vector2<double> point) {
+  sf::Vector2<double> tempCoord = sf::Vector2<double>(point.x, point.y);
+  
+  // Normalize the coordinate to our map's origin
+  tempCoord.x = point.x - anchorPoint.x;
+  tempCoord.y = point.y - anchorPoint.y;
+  
+  // Normalize the coordinate to our map's tile width
+  tempCoord.x = tempCoord.x/tileWidth;
+  tempCoord.y = tempCoord.y/tileWidth;
+  
+  // Normalize the entire coordinate into a 1D index
+  return tempCoord.y*size.x + tempCoord.x;
+}
 
 // Public
 void Map::setTile(sf::Vector2<int> indexes, sf::Sprite spr, type typeOfTile) {
-  Tile tempTile = new Tile(indexes, spr, typeOfTile);
+  Tile* tempTile = new Tile(indexes, spr, typeOfTile);
+  int realIndex = indexes.y*size.x + indexes.x;
+  mapBody[realIndex] = tempTile;
+}
+
+void Map::setTile(sf::Vector2<int> indexes, Tile* tile) {
+  // Since we are smashing a 2D array into a 1D array, the real index is the number of rows down (The y pos * the row width) plus how many columns over (the x pos)
+  int realIndex = indexes.y*size.x + indexes.x;
+  mapBody[realIndex] = tile;
+}
+
+Tile* Map::getTileAtIndex(sf::Vector2<int> pos) {
+  int realIndex = pos.y*size.x + pos.x;
+  return mapBody[realIndex];
+}
+
+Tile* Map::getTileAtPoint(sf::Vector2<double> point) {
+  int realIndex = getIndexFromPoint(point);
+  return mapBody[realIndex];
+}
+
+sf::Vector2<int> Map::getSize() {
+  return size;
+}
+
+void Map::deleteTile(sf::Vector2<int> indexes) {
+  int realIndex = indexes.y*size.x + indexes.x;
+  delete mapBody[realIndex];
+  mapBody[realIndex] = NULL;
+}
+
+void Map::deleteAllTiles() {
+  for (int i = 0; i < size.x*size.y; i++) {
+    if (mapBody[i] != NULL) {
+      delete mapBody[i];
+      mapBody[i] = NULL;
+    }
+  }
 }
--- pacman/map.hpp
+++ pacman/map.hpp
@@ -6,17 +6,18 @@ private:
   sf::Vector2<int> size;
   sf::Vector2<double>anchorPoint;
   int tileWidth;
+  
+  int getIndexFromPoint(sf::Vector2<double> point);
 public:
   Map(sf::Vector2<double> anchor, sf::Vector2<int> sze, int width);
   
   void setTile(sf::Vector2<int> indexes, sf::Sprite spr, type typeOfTile); // Set a tile by index
   void setTile(sf::Vector2<int> indexes, Tile* tile);
   
-  Tile getTileAtIndex(sf::Vector2<int> pos);                               // Gets tile by array index
-  Tile getTileAtPoint(sf::Vector2<double> point);                          // Gets tile by position on screen
+  Tile* getTileAtIndex(sf::Vector2<int> pos);                              // Gets tile by array index
+  Tile* getTileAtPoint(sf::Vector2<double> point);                         // Gets tile by position on screen
+  sf::Vector2<int> getSize();                                           // Gets size as a vector
   
   void deleteTile(sf::Vector2<int> indexes);                               // Delets tile at given (x, y) index
   void deleteAllTiles();                                                   // Deletes all tiles
-  
-  void drawMap(sf::RenderWindow* window);                                  // Draws all tiles to the given window obj
 };
